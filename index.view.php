176df9a7da8a3ff8ac8753731f1cb219df5c50c3
GitHub1337User <microsoft228windows@yandex.ru>
2021-04-01 17:36:36 +0500

    Update a little bit

---
+++ index.view.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <link rel="stylesheet" href="css/main-menu.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minecraft</title>
+</head>
+<body>
+
+</body>
+</html>
