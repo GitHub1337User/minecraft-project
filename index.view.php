42153aaa8387153c1873a467968ed9d34f51df57
GitHub1337User <microsoft228windows@yandex.ru>
2021-04-04 23:51:09 +0500

    Mainpage + Basic header created

--- index.view.php
+++ index.view.php
@@ -1,22 +1,24 @@
 <!doctype html>
-<html lang="ru">
+<html>
 <head>
+    <link rel="shortcut icon" href="res/img/favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="css/main-menu.css">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Minecraft</title>
+    <title>Minecraft-Chest</title>
 </head>
 <body>
+<video src="res/videos/bg-vid2.mp4" autoplay muted loop class="bg-vid"></video>
 <div class="wrapper">
     <img src="res/img/logo.png" alt="logo" class="logo" draggable="false">
     <div class="rotate_trick"><h2 class="tricks">With a sauce!</h2></div>
     <div class="buttons">
-    <a href="#" class="button">Моды</a>
-    <a href="#" class="button">Текстурпаки</a>
-    <a href="#" class="button">Скины</a>
-    <a href="#" class="button">Карты</a>
+        <a href="/mainpage.php" class="button">Моды</a>
+        <a href="/mainpage.php" class="button">Текстурпаки</a>
+        <a href="/mainpage.php" class="button">Скины</a>
+        <a href="/mainpage.php" class="button">Карты</a>
     </div>
     <audio id="sound" src="res/sounds/Click.ogg"></audio>
 </div>
---
+++ mainpage.view.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="shortcut icon" href="res/img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/mainpage.css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Minecraft-Chest</title>
+</head>
+<body>
+<header>
+<!--    <img src="res/img/logo-site.png" alt="" class="site-logo" draggable="false">-->
+    <div class="site-logo">
+        <img src="res/img/favicon.png" alt="" class="pic-logo blick"><h1>MINECRAFT  CHEST</h1>
+    </div>
+    <nav class="buttons">
+        <a href="/mainpage.php" class="button">Моды</a>
+        <a href="/mainpage.php" class="button">Текстурпаки</a>
+        <a href="/mainpage.php" class="button">Скины</a>
+        <a href="/mainpage.php" class="button">Карты</a>
+        <a href="/mainpage.php" class="button">Авторизация</a>
+        <a href="/mainpage.php" class="button">Регистрация</a>
+
+    </nav>
+</header>
+</body>
+</html>
